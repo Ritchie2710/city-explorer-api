ae8b7aea79ace271fb8ddf9ac6e9ee42dac04f1f
git <richard.james.grove@gmail.com>
2023-11-01 15:58:48 +0000

    in

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 const express = require("express");
-
 const cors = require("cors");
-
 require("dotenv").config();
-
 const PORT = process.env.PORT || 8080;
-
 const app = express();
-
 app.use(cors());
-
 const data = require("./data/weather.json");
 
 app.get("/weather", (request, response) => {
-  const weather = response.json("weather!");
-});
+  const lat = request.query.lat;
+  const lon = request.query.lon;
+  const searchQuery = request.query.searchQuery;
 
-function findweatherByName(lat, lon) {
-  return data.filter(
-    (weather) => (weather.lat == lat && weather.lon == lon) || lat,
-    lon
-  );
-}
+  const filteredCity = data.find((city) => {
+    return city.city_name === searchQuery; //&& city.lat == lat && city.lon == lon;
+  });
+  const wrangledData = filteredCity.data.map((day) => {
+    return {
+      description: day.weather.description,
+      Date: day.daytime,
+    };
+  });
+  response.json(wrangledData);
+});
 
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
